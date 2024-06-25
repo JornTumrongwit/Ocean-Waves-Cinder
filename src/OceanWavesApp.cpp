90ad02010b4baa31e1fc9ec5a3f33ee6e514eb17
unknown <jorn1013@gmail.com>
2024-06-25 10:37:24 +0700

    Initial commit

--- src/OceanWavesApp.cpp
+++ src/OceanWavesApp.cpp
@@ -8,27 +8,36 @@ using namespace std;
 
 class OceanWavesApp : public App {
   public:
-	void setup() override;
-	void mouseDown( MouseEvent event ) override;
-	void update() override;
-	void draw() override;
+	  void	setup() override;
+	  void	draw() override;
+
+	  CameraPersp         mCam;
+	  gl::BatchRef        mRect;
+	  gl::GlslProgRef		mGlsl;
 };
 
 void OceanWavesApp::setup()
 {
-}
+	mCam.lookAt(vec3(3, 2, 3), vec3(0));
 
-void OceanWavesApp::mouseDown( MouseEvent event )
-{
-}
+	mGlsl = gl::GlslProg::create(gl::GlslProg::Format()
+		.vertex( loadAsset("render.vert") )
+		.fragment( loadAsset("render.frag") ));
 
-void OceanWavesApp::update()
-{
+	auto plane = geom::Plane().subdivisions(ivec2(30));
+	mRect = gl::Batch::create(plane, mGlsl);
+
+	gl::enableDepthWrite();
+	gl::enableDepthRead();
 }
 
 void OceanWavesApp::draw()
-{
-	gl::clear( Color( 0, 0, 0 ) ); 
+{ 
+	gl::clear(Color(0.2f, 0.2f, 0.3f));
+	gl::setMatrices(mCam);
+
+	mGlsl->uniform("uCheckSize", 30.0f);
+	mRect->draw();
 }
 
 CINDER_APP( OceanWavesApp, RendererGl )
